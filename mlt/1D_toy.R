6af5a86832c5b88b9da15e49c38a5ef3fc18f5c2
Beate Sick <bsick@users.noreply.github.com>
2020-02-19 17:50:58 +0100

    2 new data sets - horizontal, 2 lines

--- mlt/1D_toy.R
+++ mlt/1D_toy.R
@@ -1,3 +1,6 @@
+library(mlt)
+
+
 NB = 10
 
 get_data_lin = function(){
@@ -8,7 +11,13 @@ get_data_lin = function(){
 
 get_data_nx = function(){
   x = seq(0,5,length.out = 100)
-  y = rnorm(100, x*0.1 + 0.2, 0.05 + 0.02*x)
+  y = rnorm(100, mean=c(x*0.5 + 0.2), 0.05 + 0.2*x)
+  return (list(x=x, y=y, dat=data.frame(x=x, y=y), scale=1))
+}
+
+get_data_0x = function(){
+  x = seq(0,5,length.out = 100)
+  y = rnorm(100,  mean=0.2, sd=(0.05 + 0.2*x))
   return (list(x=x, y=y, dat=data.frame(x=x, y=y), scale=1))
 }
 
@@ -18,27 +27,60 @@ get_data_sin = function(){
   return (list(x=x, y=y, dat=data.frame(x=x, y=y), scale=1))
 }
 
+get_data_2l = function(){
+  n=500
+  x1 = seq(0,5,length.out = n/2)
+  x2 = seq(0,5,length.out = n/2)
+  y1 = rnorm(n/2, mean=(x1*0.1 + 0.2), 0.05 + 0.1*x)
+  y2 = rnorm(n/2, mean=(x2*(-0.5) + 0.2), 0.5  -0.1*x)
+  x = c(x1,x2)
+  y = c(y1, y2)
+  return (list(x=x, y=y, dat=data.frame(x=x, y=y), scale=1))
+}
 
+
+# daten auf 2 geraden 
+# -> get_data_2l(), NB=50 und NB=10 sieht recht verschieden aus.
+
+# horizontal, aber varianz wird grösser
+# -> get_data_0x()
+# -> je flacher die Steigung, desto schwerer wird variierende Varianz gelernt
+
+# coeff interpretieren -> # falls todist=Normal: 
+# falls todist=Normal: beta gibt change in h(y) wenn x um 1 unit steigt 
+
+# in mlt probieren dass bernstein-coef auch von x abhängen (interaction?)
+# -> mlt vignette page 37.
+# Bernstein-Koeffizienten können nur von kategorischen Variablen abhängen (?)
+# die als strata Variablen gesetzt werden, siehe argument interacting in ctm
 
 xy_dat = get_data_lin()
 xy_dat = get_data_nx()
 xy_dat = get_data_sin()
+xy_dat = get_data_0x()
+xy_dat = get_data_2l()
 
 x = xy_dat$x
 y = xy_dat$y
 dat=xy_dat$dat
-plot(x,y)
+
+delta=abs(diff(range(y)))/4
+my_min = min(y)-delta
+my_max = max(y)+delta
+
+plot(x,y, ylim=c(my_min, my_max))
 
 # LM
 fm = y ~ x
 fit_lm = lm(fm, data=dat)
+abline(fit_lm)
 fit_lm$coef
 (NLL_LM = logLik(fit_lm) / nrow(dat))
 
 # MLT
 library(mlt)
 #library(basefun)
-var_y <- numeric_var("y", support = c(0, 1))
+var_y <- numeric_var("y", support = c(my_min, my_max))
 bb <- Bernstein_basis(var_y, order=NB, ui="increasing")
 y_grid <- as.data.frame(mkgrid(bb, n = 500))
 # set up model for mlt
@@ -48,8 +90,13 @@ mlt_fit <- mlt(ctm, data = dat, verbose=TRUE)
 mlt_fit$coef
 
 preds = predict(mlt_fit, newdata=dat,  q=y_grid$y, type='density')
-plot(x,y)
-for (idx in c(1,25,50,75,90)){
+plot(x,y, ylim=c(my_min, my_max))
+strech=0.5
+n=100  # number of simulated points
+for (idx in c(n*0.1,n*0.25,n*0.5,n*0.75,n*0.9)){
   f = preds[,idx]
-  lines(f/10+x[idx],y_grid$y)
+  lines(strech*f+x[idx],y_grid$y)
+  abline(v=x[idx], lty=2)
 }
+
+
