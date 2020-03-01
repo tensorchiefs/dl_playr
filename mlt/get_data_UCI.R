2ae8f4d9870a2d374233cfa7731a86152fcbea28
Beate Sick <bsick@users.noreply.github.com>
2020-03-01 19:55:30 +0100

    get_data extended for x_scael, model_grid_UCI started for grid search

--- mlt/get_data_UCI.R
+++ mlt/get_data_UCI.R
@@ -1,5 +1,14 @@
-u_scale = function (y, min_y, max_y){
-  return ( (y-min_y)/(max_y-min_y) )
+u_scale = function (col, min_col, max_col){
+  return ( (col-min_col)/(max_col-min_col) )
+}
+
+col_scale <- function(col_train, spatz=0.05, col_test) {
+  max_col = max(col_train) * (1 + spatz)
+  min_col = min(col_train) * (1 - spatz)
+  scale = max_col - min_col
+  col_train = u_scale(col_train, min_col=min_col, max_col=max_col)
+  col_test = u_scale(col_test, min_col=min_col, max_col=max_col)
+  return (list(col_train=col_train, col_test=col_test))
 }
 
 # utils_back_scale = function(y_scale, y, spatz = 0){
@@ -8,7 +17,7 @@ u_scale = function (y, min_y, max_y){
 #   return (y_scale * (max_y - min_y) + min_y)
 # }
 
-load_data = function(path, split_num=0, spatz = 0.05) {
+load_data = function(path, split_num=0, spatz = 0.05, x_scale =TRUE) {
   idx_train = read.table(paste0(path, '/data/index_train_', split_num, '.txt')  ) + 1 #We are R-Based
   idx_test = read.table(paste0(path, '/data/index_test_', split_num, '.txt')  ) + 1 #We are R-Based
   y_col = read.table(paste0(path, '/data/index_target.txt')  )  + 1
@@ -21,18 +30,26 @@ load_data = function(path, split_num=0, spatz = 0.05) {
   y_train = y[idx_train$V1]
   X_test = X[idx_test$V1,]
   y_test = y[idx_test$V1]
+
+  y_s2 = col_scale(col_train=y_train, spatz=0.05, col_test=y_test)
+  y_train = y_s2$col_train
+  y_test = y_s2$col_test
   
-  max_y = max(y_train) * (1 + spatz)
-  min_y = min(y_train) * (1 - spatz)
-  scale = max_y - min_y
-  y_train = u_scale(y_train, min_y=min_y, max_y=max_y)
-  y_test = u_scale(y_test, min_y=min_y, max_y=max_y)
+  if(x_scale==TRUE){
+    X_train=as.matrix(X_train) # also in case of 1 x
+    for( i in 1:ncol(X_train)){
+      X_s2 = col_scale(col_train=X_train[,i], spatz=0.05, col_test=X_test[,i])
+      X_train[,i] = X_s2$col_train
+      X_test[,i] = X_s2$col_test
+      }
+    
+  }
   return (list(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, runs = runs, scale = scale))
 }
 
-get_data_boston = function(path, split_num=0, spatz = 0.05) {
+get_data_boston = function(path, split_num=0, spatz = 0.05, x_scale =TRUE) {
   name = 'boston'
-  ret = load_data(path, split_num, spatz)
+  ret = load_data(path, split_num, spatz, x_scale)
   ret$name = name
   return (ret)
 }
@@ -59,6 +76,29 @@ get_data_energy = function(path, split_num=0, spatz = 0.05) {
 }
 
 
+##### the "old" get_data function (before protocol)
+## Data set
+get_data = function () {
+  data("BostonHousing2", package = "mlbench")
+  dat=BostonHousing2
+  #str(dat)  #506 obs. of  19 variables
+  names(dat)
+  scale = max(dat$medv) - min(dat$medv)
+  dat$cmedv = NULL #Remove second version of 
+  #dat$y_obs = dat$medv
+  dat$y = utils_scale(dat$medv)
+  dat$medv = NULL
+  names(dat)
+  y = as.matrix(dat$y)
+  x = as.matrix(dat[,5:ncol(dat)]) #<------ Here ist y dabei!!!!!!!
+  datx = dat[,5:(ncol(dat)-1)]
+  print(paste0('Names in X : ',names(datx)))
+  x = as.matrix(datx) #<------ Here ist y dabei gewesen!!!!
+  #rm(dat)
+  #x is now data-matrix
+  #y is repsone matrix
+  return (list(x=x,y=y,dat=dat,scale=scale))
+}
 
 
 
---
+++ mlt/model_grid_UCI.R
@@ -0,0 +1,83 @@
+#rm(list=ls())
+library(MASS)
+library(ggplot2)
+library(mlt)
+library(basefun)
+library(keras)
+library(tensorflow)
+library(tfprobability)
+
+# source('~/Documents/workspace/dl_playr/mlt/bern_utils.R')
+#source('https://raw.githubusercontent.com/tensorchiefs/dl_playr/master/mlt/bern_utils.R')
+source('bern_utils.R')
+source("model_utils.R")
+source('get_data_UCI.R')
+
+### data loaded from db 
+offset_beate  = "c:/Users/sick/dl Dropbox/beate sick/IDP_Projekte/DL_Projekte/shared_Oliver_Beate/mlt/UCI_Datasets/"
+offset = offset_beate
+
+# get_data = get_data_protein
+# path = paste(offset, 'protein-tertiary-structure/', sep="")
+
+get_data = get_data_boston
+path = paste(offset, 'bostonHousing/', sep="")
+
+
+SCALE = TRUE
+reg_factor = 0.0 #Boston 0.05 Protein 0
+T_STEPS = 500 #w.r.t Batchsize Protein 75000, Boston 50000
+bs = -1 # boston -1, protein 128L
+flds = NULL
+runs = 5
+T_OUT = 100
+nb = 8L
+len_theta = nb + 1L
+
+grid_reg_factor = c(0.0, 0.05, 0.5)
+
+## !! TODO: use only train for grid search, grid also over bs, x_scale?
+
+for (reg_factor in grid_reg_factor){
+  
+  #Main Loop############ 
+  #
+  #run = 4 s
+  history = make_hist()
+  ret = get_data(path)
+  (runs = ret$runs)
+  for (run in 1:2 ){   # !!!!!for grid search restrict on first few folds
+    #for (run in 1:runs){ #<----------------
+    # run =1
+    ret = get_data(path, split_num=run, spatz = 0.05, x_scale = TRUE)
+    
+    print(paste0("Run ", run, ' from ', runs))
+    x_train1 = tf$Variable(ret$X_train, dtype='float32')
+    x_test = tf$Variable(ret$X_test, dtype='float32')
+    y_train1 = tf$reshape(tf$Variable(ret$y_train, dtype='float32'), c(-1L,1L))
+    y_test = tf$reshape(tf$Variable(ret$y_test, dtype='float32'), c(-1L,1L))
+    print(paste0('training ', x_train1$shape, ' testing ', x_test$shape))
+    
+    
+    source('model_7.R')
+    x_dim = ncol(x_test)
+    model_7 = new_model_7(len_theta = len_theta, x_dim = x_dim, y_range=ret$scale, 
+                          eta_term = TRUE, a_term = TRUE, reg_factor = reg_factor, bs = bs)
+    model_7$name = paste0('model_7_reg_',reg_factor, '_', ret$name)
+    history = model_train(model_7, history, x_train1, y_train1,x_test, y_test, T_STEPS = T_STEPS)
+    
+    for (i in 1:20){
+      ret = model_get_p_y(model_7, x_train1[i,,drop=FALSE], 0, 1, 100)
+      print(paste0(i, '  ',round(sum(ret$p_y)/100,3)))
+      plot(ret$y, ret$p_y, main=paste0(i,' train ', round(sum(ret$p_y)/300,3)))
+    }
+    
+  }
+  
+  
+  out = paste0(path,Sys.Date(),round(rnorm(1,mean=30,sd=5),3),'_','reg_factor_',reg_factor,'/')
+  dir.create(out)
+  save(history, file = paste0(out,'history_',T_STEPS,'.Rdata'))
+  
+ 
+}
